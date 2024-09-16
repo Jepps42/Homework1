0e3f87e139d6361344eee1d06221f43ee9641589
Jepps42 <63672521+Jepps42@users.noreply.github.com>
2024-09-16 15:58:23 -0400

    WASD Controller added/other capsule

--- ControllerAssignment/Assets/Scripts/WASD_Cap.cs
+++ ControllerAssignment/Assets/Scripts/WASD_Cap.cs
@@ -21,6 +21,32 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        //If statement to move right on screen by using D
+        if (Input.GetKey(KeyCode.D))
+        {
+            //Make rigidbody move right
+            capsule.AddForce(Vector2.right *  forceamt);
+        }
+
+        //If statement to make rigidbody move to the left
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            //Make the rigidbody go left
+            capsule.AddForce(Vector2.left * forceamt);
+        }
+
+        //If statement to make rigidbody move down
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            //Make rigidbody go down
+            capsule.AddForce(Vector2.down * forceamt);
+        }
+
+        //If statement to make rigidbody move up
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            //Make rigidboyd go up
+            capsule.AddForce(Vector2.up * forceamt);
+        }
+    }  
 }
