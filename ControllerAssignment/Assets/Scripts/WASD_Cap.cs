730b98d61cc2e9e06cfc99d3fff1c4ad41a0d4c6
Jepps42 <63672521+Jepps42@users.noreply.github.com>
2024-09-16 15:37:17 -0400

    Added controller script to capsule

--- ControllerAssignment/Assets/Scripts/WASD_Cap.cs
+++ ControllerAssignment/Assets/Scripts/WASD_Cap.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    //Variables
+    public float forceamt = 5f;
+
+    private int i = 7;
+
+    private Rigidbody2D capsule;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Find rigidbody that this script is on
+        capsule = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
