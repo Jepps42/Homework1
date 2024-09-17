a3d76928300553c102df61c13a1166e9a54661ce
Jepps42 <63672521+Jepps42@users.noreply.github.com>
2024-09-17 10:19:47 -0400

    Added speed for 2nd capsule

--- ControllerAssignment/Assets/Scripts/WASD_Transform.cs
+++ ControllerAssignment/Assets/Scripts/WASD_Transform.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class WASD_Transform : MonoBehaviour
 {
+    //Variables
+    public float speed = 5f;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Not calling any varible to find the object
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Find the object every frame and make a vector
+        Vector2 pos = transform.position;
+
+
+        //If I press right arrow, make the object move to the right
+        //If is called every time I press the right arrow
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
     }
 }
